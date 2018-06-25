2bf6b45822cbe884e3dea0aa50c2ed16a85f14c2
Robert Knauer <robert@capsaicin-dev.de>
2018-06-25 23:55:23 +0200

    implemented test suite using UnitTest++

--- test/FormattedOutput.cc
+++ test/FormattedOutput.cc
@@ -1,48 +1,50 @@
-#include <iostream>
+#include <sstream>
+#include <UnitTest++/UnitTest++.h>
 #include "FormattedOutput.hh"
 
-class TestFormattedOutput : public FormattedOutput {
+class FormattedOutputTest : public FormattedOutput {
 public:
-    int printChar(char c) override;
+    int printChar(char c) override {
+        ss << c;
+        return 0;
+    }
+    std::stringstream ss;
 };
 
-int TestFormattedOutput::printChar(char c) {
-    std::cout << c;
-    return 0;
-}
-
-int main() {
-    TestFormattedOutput fo;
-    int c;
-
-    // unsigned decimal integer
-    c = fo.printUnsignedInteger(123456789);
-    std::cout << " -> " << c << std::endl;
-
-    // unsigned hexadecimal integer
-    c = fo.printUnsignedInteger(0xdeadbeef, 16);
-    std::cout << " -> " << c << std::endl;
-    c = fo.printUnsignedInteger(0xdeadbeef, 16, true);
-    std::cout << " -> " << c << std::endl;
-
-    // invalid base
-    c = fo.printUnsignedInteger(123, 20);
-    std::cout << " -> " << c << std::endl;
-
-    // signed decimal integer
-    c = fo.printSignedInteger(-42);
-    std::cout << " -> " << c << std::endl;
-
-    // string
-    c = fo.printString("test123");
-    std::cout << " -> " << c << std::endl;
-
-    // print formatted
-    signed int c_test;
-    c = fo.printFormatted("%d %i %u %o %x %X ... %c %s %p %n %%",
-            -42, -1337, 666, 042, 0xdeadbeef, 0xbadcab1e, 'a', "test", main, &c_test);
-    std::cout << " -> " << c_test << " -> " << c << std::endl;
-    c = printf("%d %i %u %o %x %X ... %c %s %p %n %%",
-            -42, -1337, 666, 042, 0xdeadbeef, 0xbadcab1e, 'a', "test", main, &c_test);
-    std::cout << " -> " << c_test << " -> " << c << std::endl;
+SUITE (FormattedOutput) {
+    TEST (unsignedDecimalInteger) {
+        FormattedOutputTest fo;
+        CHECK_EQUAL(9, fo.printUnsignedInteger(123456789));
+        CHECK_EQUAL("123456789", fo.ss.str());
+    }
+    TEST (unsignedHexadecimalInteger) {
+        FormattedOutputTest fo;
+        CHECK_EQUAL(8, fo.printUnsignedInteger(0xdeadbeef, 16));
+        CHECK_EQUAL("deadbeef", fo.ss.str());
+        fo.ss.str("");
+        CHECK_EQUAL(8, fo.printUnsignedInteger(0xdeadbeef, 16, true));
+        CHECK_EQUAL("DEADBEEF", fo.ss.str());
+    }
+    TEST (unsignedInvalidBase) {
+        FormattedOutputTest fo;
+        CHECK_EQUAL(-1, fo.printUnsignedInteger(123, 20));
+    }
+    TEST (signedDecimalInteger) {
+        FormattedOutputTest fo;
+        CHECK_EQUAL(3, fo.printSignedInteger(-42));
+        CHECK_EQUAL("-42", fo.ss.str());
+    }
+    TEST (string) {
+        FormattedOutputTest fo;
+        CHECK_EQUAL(7, fo.printString("test123"));
+        CHECK_EQUAL("test123", fo.ss.str());
+    }
+    TEST (printf) {
+        FormattedOutputTest fo;
+        int c = 0;
+        CHECK_EQUAL(48, fo.printFormatted("%d %i %u %o %x %X %c %s %p %n %%",
+                                           -42, -1337, 666, 042, 0xdeadbeef, 0xbadcab1e, 'a', "test", 0, &c));
+        CHECK_EQUAL("-42 -1337 666 42 deadbeef BADCAB1E a test 0x0  %", fo.ss.str());
+        CHECK_EQUAL(46, c);
+    }
 }
